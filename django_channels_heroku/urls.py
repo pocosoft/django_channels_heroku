5f468f038bd13fead42833375fb3eb7f67c79aca
orikasa <a.orikasa@pocosoft.jp>
2018-06-01 10:42:10 +0900

    Fix 404.

--- django_channels_heroku/urls.py
+++ django_channels_heroku/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path, include
+from chat import views as chat_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^chat/', include('chat.urls')),
-
+    url(r'^$', chat_views.index),
 ]
